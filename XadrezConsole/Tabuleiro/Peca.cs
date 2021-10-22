5e69e112056a92c527634bc40811ada5770583e7
Daniel Goldacker <daniel-goldacker@hotmail.com>
2021-10-22 16:40:25 -0300

    Alterado nome do método para MovimentoPossivel

--- XadrezConsole/Tabuleiro/Peca.cs
+++ XadrezConsole/Tabuleiro/Peca.cs
@@ -42,7 +42,7 @@
             return false;
         }
 
-        public bool PodeMoverPara(Posicao pos)
+        public bool MovimentoPossivel(Posicao pos)
         {
             return MovimentosPosiveis()[pos.Linha, pos.Coluna];
         }
